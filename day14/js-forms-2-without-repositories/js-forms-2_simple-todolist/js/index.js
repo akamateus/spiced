c56b8ab678d058596c535d4699bce8fdbe555b19
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-06-01 02:13:05 +0200

    .

--- day14/js-forms-2-without-repositories/js-forms-2_simple-todolist/js/index.js
+++ day14/js-forms-2-without-repositories/js-forms-2_simple-todolist/js/index.js
@@ -2,3 +2,12 @@ console.clear();
 
 const form = document.querySelector('[data-js="form"]');
 const todoList = document.querySelector('[data-js="todoList"]');
+const headline = document.getElementById("headline");
+const task = document.getElementById("task");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("Headline: ", headline.value);
+  console.log(`Task: ${task.value}`);
+  e.target.reset();
+  e.target.elements.headline.focus();
+});
