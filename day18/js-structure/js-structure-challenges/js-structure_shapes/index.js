10b719aa17f44c940d8cd8f7180af62c3983772a
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-06-02 16:41:11 +0200

    added js sctructure

--- day18/js-structure/js-structure-challenges/js-structure_shapes/index.js
+++ day18/js-structure/js-structure-challenges/js-structure_shapes/index.js
@@ -1,23 +1,27 @@
 console.clear();
 
-const root = document.getElementById("root");
+import {getRandomColor} from "./utils/randomColor.js";
+import {createCircle}
 
-const circle = document.createElement("div");
-circle.classList.add("circle");
-circle.addEventListener("click", () => {
-  circle.style.backgroundColor = "#ccc";
-});
+const root = document.getElementById("root");
 
 const square = document.createElement("div");
 square.classList.add("square");
 square.addEventListener("click", () => {
-  square.style.backgroundColor = "#ccc";
+  square.style.backgroundColor = getRandomColor();
 });
 
 const pentagon = document.createElement("div");
 pentagon.classList.add("pentagon");
 pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = "#ccc";
+  pentagon.style.backgroundColor = getRandomColor();
 });
 
 root.append(circle, square, pentagon);
+root.append(circle,square, pentagon)
+
+// const circle = document.createElement("div");
+// circle.classList.add("circle");
+// circle.addEventListener("click", () => {
+//   circle.style.backgroundColor = getRandomColor();
+// });
