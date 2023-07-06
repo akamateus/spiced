13a68afa25a5615b1d8444d316efde9245e3fa7c
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-07-06 16:42:47 +0200

    added day 37

---
+++ day34/react-data-fetching/challenges-react-data-fetching/react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -0,0 +1,58 @@
+import Controls from "../Controls/index";
+import Map from "../Map/index";
+import useSWR from "swr";
+
+const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+function ISSTracker() {
+  const { data, error, isLoading, isValidating, mutate } = useSWR(URL, fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  // render data
+  return (
+    <main>
+      <span role="img" aria-label={isValidating ? "Validating" : "Ready"}>
+        {isValidating ? "🔄" : "✅"}
+      </span>
+      <Map longitude={data.longitude} latitude={data.latitude} />
+      <Controls
+        longitude={data.longitude}
+        latitude={data.latitude}
+        onRefresh={() => mutate()}
+      />
+    </main>
+  );
+}
+export default ISSTracker;
+
+// export default function ISSTracker() {
+//   const [coords, setCoords] = useState({
+//     longitude: 0,
+//     latitude: 0,
+//   });
+
+//   async function getISSCoords() {
+//     try {
+//       const response = await fetch(URL);
+//       if (response.ok) {
+//         const data = await response.json();
+//         setCoords({ longitude: data.longitude, latitude: data.latitude });
+//       }
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+
+//   useEffect(() => {
+//     const timer = setInterval(() => {
+//       getISSCoords();
+//     }, 5000);
+
+//     return () => {
+//       clearInterval(timer);
+//     };
+//   }, []);
---
+++ day34/react-data-fetching/code-class.md
@@ -0,0 +1,1 @@
+https://github.com/spiced-academy/celery-web-dev/tree/8b5b434a1502ea2fb57cfaa817ea48827580bc41/sessions/react-data-fetching/code-from-lecture
